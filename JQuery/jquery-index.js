b622e5c90d4083c2bf27a70f0d0ab0e46a05967f
Francisco Jesus <fgataval@everis.com>
2019-02-19 13:54:58 +0100

    Ajax para inicio de sesión arreglado

--- JQuery/jquery-index.js
+++ JQuery/jquery-index.js
@@ -5,19 +5,37 @@ $(document).ready(function(){
 function login() {
   var user = $("#user").val();
   var pwd = $("#pwd").val();
-  $("#fondo").show();
-  $.post("../../PHP/Sessions/Login.php",
-  {
-    Nom_User: user,
-    passwd: pwd,
-  },
-function(data, status){
-  $("#fondo").hide();
+  if(user == "" || pwd == ""){
+    if($("#datosVacios").length <= 0){
+      $("<p id='datosVacios' style='color: red; margin-top: 5px;'>Rellena los datos para poder iniciar sesión</p>").insertAfter("#iniciar_S");
+    }
+  }else{
+    $("#datosVacios").remove();
+    $("#fondo").removeAttr("display");
+    $.ajax({
+      type: "post",
+      url: "../../PHP/Sessions/Login.php",
+      data: {Nom_User: user, passwd: pwd},
+      dataType: "html",
+      success: function (response) {
+        $("#fondo").hide();
+        if(response != "correct"){
+          swal("Error", response, "error")
+        }
+        else{
+          window.location.href = "inicio.html"
+        }
+        console.log(response);
+      }
+    });
+  }
+/*function(data, status){
+$("#fondo").hide();
   if(data != "correct"){
     swal("Error", data, "error")
   }
   else{
     window.location.href = "inicio.html"
   }
-})
+}*/
 }
