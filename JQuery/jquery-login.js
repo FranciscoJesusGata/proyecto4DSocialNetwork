2480f4161f5443695dda9b452821eaed23f8d43c
Diego Montero <diegomonter747@gmail.com>
2019-03-03 12:07:51 +0100

    main page

--- JQuery/jquery-login.js
+++ JQuery/jquery-login.js
@@ -6,7 +6,7 @@ $.ajax({
     dataType: "html",
     success: function(data){
                 if (data == "yes"){
-                    window.location.replace("../../HTML/html/inicio.html");
+                    window.location.replace("../../HTML/html/main.html");
                 }
             },
     error: function(){
@@ -26,7 +26,7 @@ function login() {
     var pwd = $("#pwd").val();
     if(user.length < 1 || pwd.length < 1){
       if($("#alerta_passwd").is(":hidden")){
-            $("#alerta_passwd").toggle("slow");  
+            $("#alerta_passwd").toggle("slow");
         }
     }else{
         if(!$("#alerta_passwd").is(":hidden")){
@@ -34,7 +34,7 @@ function login() {
         }
         $("#fondo").removeAttr("display");
         $.ajax({
-            type: "post", 
+            type: "post",
             url: "../../PHP/Sessions/Login.php",
             data: {Nom_User: user, passwd: pwd},
             dataType: "html",
@@ -44,7 +44,7 @@ function login() {
                     swal("Error", response, "error");
                 }
                 else{
-                    window.location.href = "inicio.html";
+                    window.location.href = "main.html";
                 }
             }
         });
