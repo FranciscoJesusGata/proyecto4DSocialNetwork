c4157c227d79448810f823d4b3fb002c194ccdad
Diego Montero <diegomonter747@gmail.com>
2019-03-27 18:36:18 +0100

    Notificaciones parte 2

--- JQuery/jquery-navbar.js
+++ JQuery/jquery-navbar.js
@@ -25,11 +25,45 @@ function checkNotificaciones(){
   $.ajax({
     type: "POST",
     url: "/projectSocialNetwork/PHP/Querys/Notificaciones.php",
-    data: {adv: false},
-    async: true,
+    data: {adv: "msg"},
+    async: false,
+    dataType: "",
+    success: function(data){
+                if(data != "0"){
+                  $("#msg-span").text(data);
+                } else{
+                  $("#msg").hide();
+                }
+            },
+  });
+}
+
+function checkPeticionesSeguimiento(){
+  $.ajax({
+    type: "POST",
+    url: "/projectSocialNetwork/PHP/Querys/Notificaciones.php",
+    data: {adv: "seg"},
+    async: false,
     dataType: "html",
     success: function(data){
-                if(data != 0){
+                if(data != "0"){
+                  $("#seg-span").text(data)
+                } else{
+                  $("#seg").hide();
+                }
+            },
+  });
+}
+
+function checkCountNotificaciones(){
+  $.ajax({
+    type: "POST",
+    url: "/projectSocialNetwork/PHP/Querys/Notificaciones.php",
+    data: {adv: "total"},
+    async: false,
+    dataType: "html",
+    success: function(data){
+                if(data != "0"){
                   $("#count-not").text(data)
                 }
             },
@@ -38,6 +72,8 @@ function checkNotificaciones(){
 
 checkSesionNavbar();
 checkNotificaciones();
+checkPeticionesSeguimiento();
+checkCountNotificaciones();
 
 $("#cerrarSesionNavbar").click(function (){
   window.location.href = "../../PHP/Sessions/Logout.php";
--- PHP/Querys/Notificaciones.php
+++ PHP/Querys/Notificaciones.php
@@ -4,32 +4,44 @@
   session_start();
 
   function getNumMensajes($nombre, $database){
-    $sql = "SELECT count(Id_M) FROM mensajes WHERE Id_Receptor = '".$nombre."'";
+    $sql = "SELECT count(Id_M) AS num FROM mensajes WHERE Id_Receptor = '".$nombre."' AND F_Lectura is NULL";
     $datos = $database->get_data($sql);
     return $datos;
   }
 
-  $nombre = $_SESSION['N_Usuario'];
-  $advanced = $_POST['adv'];
-  $ok = $database->conexion();
+  function getPeticionesSeguimiento($nombre, $database){
+    $sql = "SELECT count(Nombre_Usuario) AS num FROM seguir WHERE Nombre_seguido = '".$nombre."' AND F_Inicio is NULL";
+    $datos = $database->get_data($sql);
+    return $datos;
+  }
 
-  if(!$advanced){
-    $field = $_POST['field']
-    switch ($field) {
-      case 'msg':
-        // code...
-        break;
+  if (isset($_POST['adv'])) {
+    $nombre = $_SESSION['N_Usuario'];
+    $advanced = $_POST['adv'];
+    $ok = $database->conexion();
+
+    if($ok == "error"){
+        echo "Error al conectar con la base de datos";
+    }else {
+      switch ($advanced) {
+        case 'msg':
+          $data = getNumMensajes($nombre, $database);
+          echo $data['num'];
+          break;
+        case 'seg':
+          $data = getPeticionesSeguimiento($nombre, $database);
+          echo $data['num'];
+          break;
+        case 'total':
+          $data1 = getPeticionesSeguimiento($nombre, $database);
+          $data2 = getNumMensajes($nombre, $database);
+          $data3 = $data1['num'] + $data2['num'];
+          echo $data3;
+          break;
+      }
     }
+  } else{
+    echo "Error al hacer la peticion";
   }
 
-  if($ok == "error"){
-      echo "Error al conectar con la base de datos";
-      exit;
-  }
-  if ($nombre == null) {
-    return null;
-  } else {
-    $data = getNumMensajes($nombre, $database);
-    echo $data[0];
-  }
 ?>
